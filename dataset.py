87a06728ba2e4eb85f4eb8d6bbdd5750990b7ebc
Biswanath-2004 <biswanathchakraborty863@gmail.com>
2026-02-21 19:35:12 +0530

    update dataset file

---
+++ dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "study_hours": [1, 2, 3, 4, 5, 6, 2, 3, 4, 5],
+    "days_left": [15, 10, 7, 5, 3, 2, 12, 8, 6, 4],
+    "difficulty": [2, 3, 4, 5, 5, 4, 3, 2, 4, 5],
+    "score": [50, 60, 65, 70, 80, 85, 55, 60, 75, 90]
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv("student_study_data.csv", index=False)
+
+print("✅ Dataset created")
